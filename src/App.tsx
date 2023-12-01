c8ce8533b555a0acc828d62f41e9ad11d1a93712
Arnaud Laforgue <arnaud.laforgue@gmail.com>
2023-12-01 12:09:15 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import { useCallback, useMemo, useState } from 'react'
+import './App.css'
+import { Die } from './components/Die'
+
+function App() {
+  const [diceValues, setValues] = useState([2, 5, 6]);
+
+  const sum = useMemo(() => diceValues.reduce((a, b) => a + b, 0), [diceValues])
+
+  const handleRoll = useCallback(
+    (diceIndex: number, newValue: number) => {
+      setValues((prev) => {
+        const newValues = [...prev]
+        newValues[diceIndex] = newValue
+        return newValues
+      })
+    },
+    []
+  )
+
+  return (
+    <>
+      {diceValues.map((valeur, index) => (
+        <Die 
+          key={index}
+          value={valeur} 
+          onRoll={(value: number) => handleRoll(index, value)} 
+        />
+      ))}
+      <div className="sum">Sum: {sum}</div>
+    </>
+  )
+}
+
+export default App
